e21c9eafbf2fde4284b527e75b418130384d5bc9
Richard <ricky201325@gmail.com>
2024-10-19 04:08:51 -0300

    primer commit

---
+++ carro/context_processor.py
@@ -0,0 +1,8 @@
+def importe_total_carro(request):
+    total = 0
+    if request.user.is_authenticated:  # No se deben usar paréntesis
+        # Verifica que 'carro' esté en la sesión
+        if 'carro' in request.session:
+            for key, value in request.session['carro'].items():
+                total += float(value["precio"])
+    return {'importe_total_carro': total}
